74bc90dee3a618057c32bd4b12e62e37d8d44c21
bagaswahyu <202210715292@mhs.ubharajaya.ac.id>
2022-10-28 08:45:49 +0700

    Tugas2 sudah selesai
    kirim

---
+++ flyp2/no.2.py
@@ -0,0 +1,5 @@
+# pencabangan IF dengan 1 kasus
+nama =input("Masukkan Nama : ")
+if nama == "Bagas Wahyu Permana" :
+    print("Hi my friend", nama, "welcome")
+    print()
---
+++ flyp4/no.4.py
@@ -0,0 +1,18 @@
+# pengulangan pakai for
+for i in range(6):
+    print("pengulangan ke - ", i)
+
+print("\n")
+
+listMakanan = ["Pepaya", "Buah Naga", "Anggur", "Strowberi", "Nanas"]
+for Makanan in listMakanan :
+    print(Makanan)
+
+print("\n")
+
+# pengulangan pakai while
+angka = 1
+
+while angka > 10 :
+    angka += 1
+    print("contoh penggunaan sintax while")
